7b743f1a65a4d993d89924dec7e08db7831edd6e
lixinyang <623270307@qq.com>
2018-12-22 22:43:47 +0800

    update:完善歌手列表页

--- vue-music-app/src/components/singer/singer.vue
+++ vue-music-app/src/components/singer/singer.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="singer-content">
     <singer-list v-if="singerList.length">
-      <cube-index-list :data="singerList"></cube-index-list>
+      <cube-index-list :data="singerList">
+          <cube-index-list-group
+            v-for="(group, index) in singerList"
+            :key="index"
+            :group="group">
+            <cube-index-list-item
+              v-for="(item, index) in group.items"
+              :key="index"
+              :item="item"
+              >
+              <div class="custom-item">
+                <div class="singer-icon">
+                  <img :src="item.avatar" height="50" width="50">
+                </div>
+                <div class="singer-name">
+                  {{item.name}}
+                </div>
+              </div>
+            </cube-index-list-item>
+          </cube-index-list-group>
+      </cube-index-list>
     </singer-list>
     <loading :flg="flg"></loading>
   </div>
 </template>
 <script>
 import { getSingerList, ERR_OK } from 'api/singer.js'
+import Singer from 'common/js/singers.js'
 import SingerList from 'base/singerList/singerList.vue'
 import Loading from 'base/loading/loading'
 const HOT_NAME = '热门'
@@ -43,10 +64,10 @@ export default {
       let map2 = {}
       list.forEach((item, index) => {
         if (index < HOT_SINGER_LENGTH) {
-          map.items.push({
+          map.items.push(new Singer({
             name: item.Fsinger_name,
             value: item.Fsinger_mid
-          })
+          }))
         }
         let k = item.Findex
         if (!map2[k]) {
@@ -55,10 +76,10 @@ export default {
             items: []
           }
         }
-        map2[k].items.push({
+        map2[k].items.push(new Singer({
           name: item.Fsinger_name,
           value: item.Fsinger_mid
-        })
+        }))
       })
       hot.push(map)
       // 数据处理后需要排序
@@ -71,6 +92,7 @@ export default {
         return a.name.charCodeAt(0) - b.name.charCodeAt(0)
       })
       this.singerList = hot.concat(ret)
+      console.log(this.singerList)
     }
   },
   created () {
@@ -93,7 +115,16 @@ export default {
   height: 70px
   line-height: 70px
   padding: 0 16px
-  font-size: $fontsize-medium
+  display: flex
+  align-items: center
+  .singer-name
+    font-size: $font-size-medium
+    margin-left: 10px
+  .singer-icon
+    height: 50px
+    width: 50px
+    border-radius: 50%
+    overflow: hidden
 // 用自定义样式，覆写内置的默认样式
 .cube-index-list-content
   background-color: #222
@@ -104,7 +135,7 @@ export default {
   line-height: 30px
   padding: 0 0 0 20px
 .cube-index-list-nav
-  right: 10px
+  right: 0
   padding: 20px 0
   border-radius: 10px
   background: rgba(0,0,0,.3)
